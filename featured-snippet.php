c2185fd48bc1ad491834c042600afb0e50c2b1e2
pixeldreher <Pixeldreher@Andres-MBP-2.fritz.box>
2017-03-17 17:35:35 +0100

    Update Function

--- featured-snippet.php
+++ featured-snippet.php
@@ -4,12 +4,34 @@ Plugin Name: Featured-Snippet-Shortcode
 Description: Use [featured_snippet image="Image URL" image_alt="Alt-Attribute for your image"]Your content for the featured snippet[/featured_snippet] to display a little featured snippet box in your content area.
 Author: TechnicalSEO.de, André Goldmann
 Author URI: https://www.technicalseo.de
-Version: 1.0
+Version: 1.1
 */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if ( !class_exists('WP') ) {
+	header('Status: 403 Forbidden');
+	header('HTTP/1.1 403 Forbidden');
+	exit();
 }
+
+function my_plugin_update_handler( EUAPI_Handler $handler = null, EUAPI_Item_Plugin $item ) {
+
+	if ( 'featured-snippet-plugin/featured-snippet-plugin.php' == $item->file ) {
+
+		$handler = new EUAPI_Handler_GitHub( array(
+			'type'       => $item->type,
+			'file'       => $item->file,
+			'github_url' => 'https://github.com/pixeldreher/featured-snippet-plugin',
+			'http'       => array(
+				'sslverify' => false,
+			),
+		) );
+
+	}
+
+	return $handler;
+
+}
+add_filter( 'euapi_plugin_handler', 'my_plugin_update_handler', 10, 2 );
 
 function featured_snippet_function() {
   	echo '<link rel="stylesheet" href="'.plugins_url('', __FILE__).'/featured-snippet.css" type="text/css" media="screen" />';
--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: pixeldreher
 Tags: seo, featured snippet, serps, serp, google, search engine
 Donate link: https://www.technicalseo.de
-Requires at least: 3.8
+Requires at least: 3.7
 Tested up to: 4.7.3
-Stable tag: 1.0
+Stable tag: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -15,4 +15,6 @@ This featured snippet plugin for WordPress integrate a small and flexible conten
 2. Use this shortcode in your wysiwyg editor: [featured_snippet image=\"Image URL\" image_alt=\"Alt-Attribute for your image\"]Your content for the featured snippet[/featured_snippet]
 
 == Changelog ==
+1.1 Update function
+
 1.0 First commit
